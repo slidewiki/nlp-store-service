29ee3881643fce2303f51bfc220fc534d8a134f3
Ben Wulff <benjamin.wulff@iais.fraunhofer.de>
2017-06-13 04:25:20 +0200

    Fixing image building

--- travis_scripts/dockerhub.sh
+++ travis_scripts/dockerhub.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-docker build -t slidewiki/nlpstore ./
+docker build -t slidewiki/nlpstore:latest-dev ./
 docker push slidewiki/nlpstore:latest-dev
